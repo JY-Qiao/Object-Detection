a2c788cfec8b681f85a35457edbcd73da84ac690
JY-Qiao <91612590+JY-Qiao@users.noreply.github.com>
2022-11-26 15:13:10 +0800

    Delete Centernet directory

--- Centernet/test.py
+++
@@ -1,8 +0,0 @@
-from nets.centernet import centernet
-
-if __name__ == "__main__":
-    model = centernet([512,512,3], 20, backbone='resnet50')
-    model.summary()
-
-    for i,layer in enumerate(model.layers):
-        print(i,layer.name)
